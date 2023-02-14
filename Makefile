2c0a449ede2297c7f28299a3dbe29257bfa480cb
johanla0 <idlhero@gmail.com>
2023-02-14 23:11:20 +0300

    Update tests

--- Makefile
+++ Makefile
@@ -4,11 +4,17 @@ branch:
 	git checkout $(ARGS) > /dev/null 2>&1 || git checkout -b $(ARGS)
 install:
 	bundle install
+lint:
+	rubocop
 pull:
 	git pull origin $(BRANCH)
 push:
 	git push origin $(BRANCH)
+test:
+	rake test $(ARGS)
 uncommit:
 	git reset --soft HEAD^
 upd:
 	git merge master --no-edit
+
+.PHONY: test
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -2,12 +2,46 @@
 
 require "test_helper"
 
+User = Struct.new(:name, :job, keyword_init: true)
+
 class TestHexletCode < Minitest::Test
-  def test_that_it_has_a_version_number
+  def setup
+    @user = User.new name: "rob"
+  end
+
+  def test_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_builds_unpaired_empty_tag
+    result = ::HexletCode::Tag.build('br')
+    assert_equal '<br>', result
+  end
+
+  def test_it_builds_unpaired_tag_with_attributes
+    result = HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+    assert_equal "<input type=\"submit\" value=\"Save\">", result
+  end
+
+  def test_it_builds_paired_tag_with_content
+    result = HexletCode::Tag.build('label') { 'Email' }
+    assert_equal "<label>Email</label>", result
+  end
+
+  def test_it_builds_paired_empty_tag
+    result = HexletCode::Tag.build('div')
+    assert_equal "<div></div>", result
+  end
+
+  def test_it_builds_form_tag
+    result = HexletCode.form_for @user do |f|
+    end
+    assert_equal "<form action=\"#\" method=\"post\"></form>", result
+  end
+
+  def test_it_builds_form_tag_with_url
+    result = HexletCode.form_for @user, url: '/users' do |f|
+    end
+    assert_equal "<form action=\"/users\" method=\"post\"></form>", result
   end
 end

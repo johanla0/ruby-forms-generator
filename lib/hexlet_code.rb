48bbeb012510b57c161bc27e006383e354fea6d8
johanla0 <idlhero@gmail.com>
2023-02-16 16:13:28 +0300

    Add Form class and form_for method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -43,11 +43,39 @@ module HexletCode
     end
   end
 
+  class Form
+    attr_accessor :action, :method, :tags, :object
+
+    def initialize(object, action, &block)
+      action ||= "#"
+      @object = object
+      @action = action
+      @method = "post"
+      @tags = []
+
+      block.call(self) if block_given?
+    end
+
+    def input(key, options = {})
+      value = @object.public_send(key)
+      as = options[:as]
+
+      @tags << if !as.nil? && as.to_sym == :text
+                 Tag.build("textarea", { name: key }.merge(options.except(:as))) { value }
+               else
+                 Tag.build("input", { name: key, type: "text", value: }.merge(options.except(:as)))
+               end
+    end
+
+    def to_s
+      Tag.build("form", action: @action, method: @method) { @tags.join }
+    end
+  end
+
   class << self
-    def form_for(_object, options = {})
-      url = options[:url]
-      url ||= "#"
-      Tag.build("form", action: url, method: "post")
+    def form_for(object, options = {}, &)
+      form = Form.new(object, options[:url], &)
+      form.to_s
     end
   end
 end

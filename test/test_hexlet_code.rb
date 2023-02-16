131f4a86c1e6a52533b479043f27b0be86419d9e
johanla0 <idlhero@gmail.com>
2023-02-16 17:28:49 +0300

    Update tests

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -35,15 +35,29 @@ class TestHexletCode < Minitest::Test
 
   def test_it_builds_form_tag
     result = HexletCode.form_for @user do |f|
-      f.input :name, class: "user-input"
-      f.input :job
+      f.input :name
     end
-    assert_equal "<form action=\"#\" method=\"post\"><input name=\"name\" type=\"text\" value=\"rob\" class=\"user-input\"><input name=\"job\" type=\"text\" value=\"hexlet\"></form>", result
+    assert_equal "<form action=\"#\" method=\"post\"><label for=\"name\">Name</label><input name=\"name\" type=\"text\" value=\"rob\"></form>", result
+  end
+
+  def test_it_builds_form_tag_without_label
+    result = HexletCode.form_for @user do |f|
+      f.input :name, class: "user-input", label: false
+    end
+    assert_equal "<form action=\"#\" method=\"post\"><input name=\"name\" type=\"text\" value=\"rob\" class=\"user-input\"></form>", result
+  end
+
+  def test_it_builds_form_tag_with_submit
+    result = HexletCode.form_for @user do |f|
+      f.input :name, label: false
+      f.submit
+    end
+    assert_equal "<form action=\"#\" method=\"post\"><input name=\"name\" type=\"text\" value=\"rob\"><input type=\"submit\" value=\"Save\"></form>", result
   end
 
   def test_it_builds_form_tag_with_url
     result = HexletCode.form_for @user, url: "/users" do |f|
-      f.input :job, as: :text, rows: 50, cols: 50
+      f.input :job, as: :text, rows: 50, cols: 50, label: false
     end
     assert_equal "<form action=\"/users\" method=\"post\"><textarea name=\"job\" rows=\"50\" cols=\"50\">hexlet</textarea></form>", result
   end
